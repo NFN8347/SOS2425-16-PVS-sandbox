f5de194ec892d1364d546f04ef8716825b7899fe
NFN8347 <pablovsc2004@gmail.com>
2025-02-21 12:32:25 +0100

    lanzando a internet

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("supadre");
+const express = require("express");
+const cool_ascii_faces = require("cool-ascii-faces");
+const app = express();
+const PORT = process.env.PORT || 16078; 
+
+app.get("/hello", (request,response)=>{response.send(cool_ascii_faces());});
+
+app.use("/",express.static("./static"));
+
+app.get("/cool", (request,response)=>{response.send(cool_ascii_faces());});
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}...`);
+});
+
+console.log('finish setup'); //finish setup saldra antes que el mensaje e Server running.. porque js no espera a q 
+// termine la funcion anterior
+
+//Tenemos un servidor web operativo en menos de 20 lineas con node
